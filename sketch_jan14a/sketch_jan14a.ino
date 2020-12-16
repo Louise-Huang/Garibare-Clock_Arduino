2d0864f85a1d9d858afa891eae2bc96735900dbe
Louise <huei.yuan1996@gmail.com>
2020-12-16 14:03:33 +0800

    first commit

---
+++ sketch_jan14a/sketch_jan14a.ino
@@ -0,0 +1,19 @@
+const int switchPin=2;
+const int motorPin=9;
+int switchState=0;
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(motorPin,OUTPUT);
+  pinMode(switchPin,INPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  switchState=digitalRead(switchPin);
+  if(switchState==HIGH){
+    digitalWrite(motorPin,HIGH);
+  }
+  else{
+    digitalWrite(motorPin,LOW);
+  }
+}
